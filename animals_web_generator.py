008b3b8344a67c3d0344dbaa1fa200414148abe2
amirdehestani <amirmo800@gmail.com>
2024-12-09 11:12:30 +0100

    add serialize_animal function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,29 +1,41 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """Loads a JSON file."""
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
-animals_data = load_data('animals_data.json')
-output = ''
-for item in animals_data:
+def serialize_animal(animal_obj):
+    """Serializes an animal object into an HTML list item."""
+    output = ''
     output += f"<li class='cards__item'>\n"
-    output += f'<div class="{item["name"]}">{item["name"]}</div>\n'
-    output += f'<p class="card__text">\n'
-    output += f"<strong>Diet: </strong>{item['characteristics']['diet']}<br/>\n"
-    output += f"<strong>Location: </strong>{item['locations'][0]}<br/>\n"
-    if 'type' in item['characteristics']:
-        output += f"<strong>Type: </strong>{item['characteristics']['type']}<br/>\n"
-    output += f"</p>\n"
+    output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
+    output += f'  <p class="card__text">\n'
+    output += f"    <strong>Diet: </strong>{animal_obj['characteristics']['diet']}<br/>\n"
+    output += f"    <strong>Location: </strong>{animal_obj['locations'][0]}<br/>\n"
+    if 'type' in animal_obj['characteristics']:
+        output += f"    <strong>Type: </strong>{animal_obj['characteristics']['type']}<br/>\n"
+    output += f"  </p>\n"
     output += f"</li>\n"
+    return output
 
-with open("animals_template.html", "r") as template_file:
-    template_html = template_file.read()
+def main():
+    animals_data = load_data('animals_data.json')
+    output = ""
+    for animal in animals_data:
+        output += serialize_animal(animal)
 
-updated_html = template_html.replace("__REPLACE_ANIMALS_INFO__", f"{output}")
+    with open("animals_template.html", "r") as template_file:
+        template_html = template_file.read()
 
-with open("animals.html", "w") as output_file:
-    output_file.write(updated_html)
+    updated_html = template_html.replace("__REPLACE_ANIMALS_INFO__", output)
 
-print("HTML file updated successfully!")
+    with open("animals.html", "w") as output_file:
+        output_file.write(updated_html)
+
+    print("HTML file updated successfully!")
+
+
+
+if __name__ == "__main__":
+    main()
